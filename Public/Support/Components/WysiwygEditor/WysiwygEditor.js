c0bbc17df9f3b9453b0aab3dac6f317c797a0343
cention-assad-zaman <assad_zaman@cention.se>
2012-10-15 13:10:12 +0600

    FS#3326 - WysiwygEditor - Change Insert Video toolbar button to Insert HTML and FS#3325 - WysiwygEditor - Close Video popup when clicking other toolbar buttons

--- Public/Support/Components/WysiwygEditor/WysiwygEditor.js
+++ Public/Support/Components/WysiwygEditor/WysiwygEditor.js
@@ -994,7 +994,8 @@ function WysiwygEditorObject() {
 				WysiwygEditor.addToolbarItemGroup(row, textareaName + '-toolbar-content', function( group ) {
 					WysiwygEditorLinkToolbarItem(self, group);
 					WysiwygEditorImageToolbarItem(self, group);
-					WysiwygEditorVideoToolbarItem(self, group);
+					//WysiwygEditorVideoToolbarItem(self, group);
+					WysiwygEditorHTMLToolbarItem(self, group);
 					WysiwygEditorHorizontalLineToolbarItem(self, group);
 				});
 				WysiwygEditorFontToolbarDropDown(self, row);
@@ -1624,17 +1625,19 @@ function WysiwygEditorLinkToolbarItem( editor, group ) {
 	});
 }
 
-function WysiwygEditorVideoToolbarItem( editor, group ) {
+
+function WysiwygEditorHTMLToolbarItem( editor, group ) {  
 	var urlTextfield=null;
 	var heightTextfield=null;
 	var widthTextfield=null;
-	WysiwygEditor.addToolbarItem(group, 'video', '', uriForServerImageResource('Components/WysiwygEditor/video.png'), I('Insert Video'), false, editor, function( item ) {
-		if( editor.videoPopup == undefined ) {
+	var htmlTextArea=null;
+	WysiwygEditor.addToolbarItem(group, 'insertHTML', '', uriForServerImageResource('Components/WysiwygEditor/insert_html.png'), I('Insert HTML'), false, editor, function( item ) {
+		if( editor.insertHTMLPopup == undefined ) {
 			
-			editor.videoPopup = WysiwygEditor.createElement('div', function( div ) {
+			editor.insertHTMLPopup = WysiwygEditor.createElement('div', function( div ) {
 				div.className = 'WysiwygEditorItemPopup';
 				div.style.display = 'none';
-				div.style.width = '450px';
+				div.style.width = '430px';
 				div.style.fontSize='10px';
 				
 				div.style.marginBottom = '2px';
@@ -1648,86 +1651,24 @@ function WysiwygEditorVideoToolbarItem( editor, group ) {
 					
 				}));
 				
-				div.appendChild(WysiwygEditor.createElement('span', function( span ) {
-					span.style.marginLeft='5px';
-					span.innerHTML = I('Embed Url:');
-					span.style.marginRight='50px';
-					
-				}));
-				
-				div.appendChild(WysiwygEditor.createElement('input', function( input ) {
-					input.setAttribute('type', 'text');
-					input.setAttribute('maxlength', 2000);
-					input.style.width = '73%';
-					urlTextfield = input;
-				}));
-				
-				
-				
-				div.appendChild(WysiwygEditor.createElement('div', function( div ) {
-					
-					div.style.height='20px';
-					div.style.display='block';
-					
-				}));
 				
 				
 				
 				div.appendChild(WysiwygEditor.createElement('span', function( span ) {
-					span.style.marginLeft='5px';
-					span.innerHTML = I('Height:');
-					span.style.marginRight='68px';
+					span.style.marginLeft='10px';
 					
-				}));
-				  
-				
-				div.appendChild(WysiwygEditor.createElement('input', function( input ) {
-					input.setAttribute('type', 'text');
-					input.setAttribute('maxlength', 3);
-					input.setAttribute('value', 150);
-					input.style.width = '30%';
-					heightTextfield = input;
-				
-				}));
-				
-				div.appendChild(WysiwygEditor.createElement('span', function( span ) {
-					span.style.marginLeft='2px';
-					span.innerHTML = I('px');
-					
-				}));
-				
-				
-				
-				div.appendChild(WysiwygEditor.createElement('div', function( div ) {
-					
-				  div.style.height='10px';
-				  div.style.display='block';
-					
-				}));
-				
-				div.appendChild(WysiwygEditor.createElement('span', function( span ) {
-					span.style.marginLeft='5px';
-					span.innerHTML = I('Width:');
-					span.style.marginRight='70px';
-					
-				}));
-				
-				div.appendChild(WysiwygEditor.createElement('input', function( input ) {
-					input.setAttribute('type', 'text');
-					input.setAttribute('maxlength', 3);
-					input.setAttribute('value', 250);
-					input.style.width = '30%';
-					input.style.marginLeft='2px';
-					widthTextfield = input;
-				
-			      }));
-				
-				div.appendChild(WysiwygEditor.createElement('span', function( span ) {
-					span.style.marginLeft='2px';
-					span.innerHTML = I('px');
 					  
 				}));
 				
+				
+				div.appendChild(WysiwygEditor.createElement('textarea', function( ta ) {
+					ta.style.width = '410px';
+					ta.style.height = '80px';
+					
+					htmlTextArea = ta;
+				
+			      }));
+				// end of new add after 10 oct 2012 
 				
 						
 				
@@ -1736,94 +1677,76 @@ function WysiwygEditorVideoToolbarItem( editor, group ) {
 				div.appendChild(WysiwygEditor.createItemPopupFooter(function( footer, footerContainer ) {
 					
 					WysiwygEditor.addItemPopupFooterButton(footer, I('Insert'), uriForApplicationImageResource('submit_infoga.png'), '#96D754', function() {
-						// add iframe for youtube
-						
-						
-						var node = WysiwygEditor.createElement('iframe', function( iframe ) {
-									
-									iframe.src = urlTextfield.value;//'http://www.youtube.com/embed/wuDA6nMHIlU';
-									
-									
-									if (widthTextfield.value<=1000 && widthTextfield.value>0)
-									{
-									    iframe.width=widthTextfield.value;
-									}
-									else
-									{
-									  iframe.width=250;
-									}
-									
-									if (heightTextfield.value<=1000 && heightTextfield.value>0)
-									{
-									    iframe.height=heightTextfield.value;
-									}
-									else
-									{
-									  iframe.height=150;
-									}
-									
-									
+						// edit on 11 oct 2012
 							
-									
-								}, editor.iframeDocument);
+						var node = WysiwygEditor.createElement('span', function( span ) {
+								
+								span.innerHTML = htmlTextArea.value;
+							}, editor.iframeDocument);
+							var selection = rangy.getIframeSelection(editor.iframe);
+							var range = editor.latestSelectionRange;
+							range.collapse(false);
+							range.insertNode(node);
+							range.collapseAfter(node);
+							selection.setSingleRange(range);
+							
+							editor.fireEvent('change');
+							
+							editor.hideInsertHTMLPopup();
 						
-						
-						
-								var selection = rangy.getIframeSelection(editor.iframe);
-								var range = editor.latestSelectionRange;
-								range.collapse(false);
-								range.insertNode(node);
-								range.collapseAfter(node);
-								selection.setSingleRange(range);
-								editor.fireEvent('change');
-						
-						
-						editor.hideVideoPopup();
 					});
+						
+					
+						
 					WysiwygEditor.addItemPopupFooterButton(footer, I('Cancel'), uriForApplicationImageResource('submit_arrow_right.png'), '#FCAB46', function() {
-						editor.hideVideoPopup();
+						editor.hideInsertHTMLPopup();
 					});
 				}));
 			});
-			editor.hideVideoPopup = function() {
-				Element.hide(editor.videoPopup);
+			editor.hideInsertHTMLPopup = function() {
+				Element.hide(editor.insertHTMLPopup);
 				item.className = 'WysiwygEditorToolbarItem';
 				
 				
 			};
-			document.body.appendChild(editor.videoPopup);
-			editor.videoUrlTextfield=urlTextfield;
+			document.body.appendChild(editor.insertHTMLPopup);
 			
-			editor.videoWidthTextfield=widthTextfield;
-			editor.videoHeightTextfield=heightTextfield;
+			editor.insertHTMLHTMLTextfield=htmlTextArea;
+			
 			
 		}
 		
-		if( Element.visible(editor.videoPopup) ) {
-			editor.hideVideoPopup();
+		if( editor.hideLinkPopup ) {
+			editor.hideLinkPopup();
+		}
+		if( editor.hideImagePopup ) {
+			editor.hideImagePopup();
+		}
+		
+		if( Element.visible(editor.insertHTMLPopup) ) {
+			editor.hideInsertHTMLPopup();
 			
 		} else {
 			if( !editor.latestSelection ) {
 				editor.updateSelection();
 			}
 			
-			editor.videoUrlTextfield.value='';
-			editor.videoWidthTextfield.value=250;
-			editor.videoHeightTextfield.value=150;
 			
-			Element.clonePosition(editor.videoPopup, item, {
+			editor.insertHTMLHTMLTextfield.value='';
+			
+			Element.clonePosition(editor.insertHTMLPopup, item, {
 					setWidth: false,
 					setHeight: false,
-					offsetLeft: 0 - (Element.getWidth(editor.videoPopup) / 2),
+					offsetLeft: 0 - (Element.getWidth(editor.insertHTMLPopup) / 2),
 					offsetTop: Element.getHeight(item.parentNode) 
 				});
-			Element.show(editor.videoPopup);
+			Element.show(editor.insertHTMLPopup);
 			item.className = 'WysiwygEditorToolbarItemActive';
 		}
+		
 	}, function( editor, item ) {
 	});
 }
-
 function WysiwygEditorImageToolbarItem( editor, group ) {
 	WysiwygEditor.addToolbarItem(group, 'image', '', uriForServerImageResource('Components/WysiwygEditor/image.png'), I('Insert image'), false, editor, function( item ) {
 		if( editor.imagePopup == undefined ) {
