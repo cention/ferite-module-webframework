cention/ferite-module-webframework

commit 8078472d374182a5b217ce4f5869f9c3107297b5

fs#4003 - MCAM.js: use GET for POST with zero data
This avoids the following message in webserver-error.log for each ajax
request that has no data:

[Fri Dec 19 09:14:48 2014] [error] [client 127.0.0.1] mod_ferite: Post without data., referer: http://localhost/Cention/workflow/

Public/Resources/Javascript/MCAM.js
@@ -160,7 +160,7 @@ function MCAM() { // Multiple Channel AJAX Mechanism
 		var requesterEvent = {};
 		var requester = this.createRequestObject();
 		requesterEvent.requester = requester;
-		requester.open( "POST", url ); 
+		requester.open( parameters.length > 0 ? "POST":"GET", url );
 		requester.setRequestHeader( 'Content-Type','application/x-www-form-urlencoded' );
 		requester.onreadystatechange = function() { 
 			self.handleEvent(requesterEvent);
@@ -237,7 +237,7 @@ function MCAM() { // Multiple Channel AJAX Mechanism
 		var requester = this.createRequestObject();
 		requesterEvent.requester = requester;
 		requesterEvent.mcamURL = url;
-		requester.open( "POST", url ); 
+		requester.open( parameters.length > 0 ? "POST":"GET", url );
 		requester.setRequestHeader( 'Content-Type','application/x-www-form-urlencoded' );
 		requester.onreadystatechange = function() { 
 			self.handleEvent(requesterEvent);

commit 650a1c146416a5ae5003ccb35b527ef1a3893075

fs#4003 - Bugfix "Turn Chat Off/On" was broken by commit 8078472d37
Changes done in commit 8078472d37 (fs#4003 - MCAM.js: use GET for POST with zero data)
will break the "Turn Chat Off/On" ticker when it changes the
XMLHttpRequest method to GET when the "parameters" array is empty. The
method used in MCAM.fireBackgroundEvent must always be POST because it
always send data in addition to the values in the parameters array.

Public/Resources/Javascript/MCAM.js
@@ -160,7 +160,7 @@ function MCAM() { // Multiple Channel AJAX Mechanism
 		var requesterEvent = {};
 		var requester = this.createRequestObject();
 		requesterEvent.requester = requester;
-		requester.open( parameters.length > 0 ? "POST":"GET", url );
+		requester.open( "POST", url );
 		requester.setRequestHeader( 'Content-Type','application/x-www-form-urlencoded' );
 		requester.onreadystatechange = function() { 
 			self.handleEvent(requesterEvent);