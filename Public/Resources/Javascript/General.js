480d8f7d09482038ce691012653f32dbc25470eb
cention-shiju <shiju.jayanand@cention.asia>
2015-02-17 16:26:22 +0800

    FS#3997 - Add function for agent to set an expected done date - Done Date cannot be past date

--- Public/Resources/Javascript/General.js
+++ Public/Resources/Javascript/General.js
@@ -506,6 +506,18 @@ function CalendarPopup( id, start ) {
 					 '<body onload="init();" style="margin:0px;">' + 
 						'<div style="text-align:center; margin-top:5px;" id="CalendarContainer"></div>' +
 					 '</body></html>');
+
+	w.onbeforeunload = function(){
+		if(id == 'DoneDate'){
+			var timestamp = Number(Math.floor(Date.now() / 1000));
+			var daystart = timestamp - (timestamp % 86400);
+				if($('DoneDate').value < daystart && $('DoneDate').value != 0){
+					$('DoneDateHuman').value = '';
+					$('DoneDate').value = daystart;
+					alert('Done Date can only be a future date');
+				}
+		}
+	}
 	w.document.close();
 }
 
