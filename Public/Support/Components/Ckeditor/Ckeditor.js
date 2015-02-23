4e3af400693fa483bb2b992b5ab0bda4b46ed0cb
Tobias Eriksson <tobias.eriksson@cention.se>
2015-02-23 11:34:52 +0100

    Revert "Ckeditor: Add attribute language"
    This reverts commit 910736b80d960ef5f4f33ba8a8a365d73cbb4cd1.

--- Public/Support/Components/Ckeditor/Ckeditor.js
+++ Public/Support/Components/Ckeditor/Ckeditor.js
@@ -109,11 +109,7 @@ function ComponentCkeditor( id ) {
 		],
 		spellCheckLanguages: []
 	};
-
-	self.setLanguage = function( language ) {
-		self.config.language = language;
-	}
-
+	
 	self.setFontSize = function( size ){
 		if( size ){
 			self.config.fontSize_defaultLabel = '' + size;
