8d685e52c48e1e39465dea2ec310c9c9d7989e32
Mujibur Rahman <mujibur.rahman@cention.se>
2015-05-05 15:45:20 +0800

    Add a global-config Js file which acquired the configs for webroot, Js path, image path for React components

---
+++ Public/Resources/Javascript/global-config.js
@@ -0,0 +1,8 @@
+/* We have to setup the global variables which will be used for web application. 
+  As now everything comes from webframework globally so I put it here temprarily.
+  When we completely moved back ferite webframework then we setup it to main.js file
+*/
+var scriptSrcPath = "/Cention.app/Resources/ReactSrc/";
+var imageSrcPath = "/Cention.app/Resources/Images/";
+var webRoot = "/Cention/web/";
+var controllerPath = "/Cention/";
