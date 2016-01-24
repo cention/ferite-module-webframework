bdf3732c746876b31ca4abb5404191269fc4760b
Sany Liew <sany.liew@cention.se>
2016-01-24 18:18:50 +0800

    Add thread (goroutine) aware feature object
    Using normal feature object to pass around multiple goroutine will
    create data race problem. This feature object satisfy featurer and
    can be used to pass around different goroutines.

---
+++ go/src/c3/feature/thread.go
@@ -0,0 +1,92 @@
+package feature
+
+import wf "c3/osm/webframework"
+
+type gofeature struct {
+	*Feature
+	ch chan struct{}
+}
+
+// threaded feature is thread aware feature.
+func NewTF() Featurer {
+	return &gofeature{
+		Feature: New(),
+		ch:      make(chan struct{}, 1),
+	}
+}
+
+// ClearCache clears the cached features that were loaded from the object server.
+func (t *gofeature) ClearCache() {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	t.Feature.ClearCache()
+}
+
+func (t *gofeature) Preload() {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	t.Feature.Preload()
+}
+
+func (t *gofeature) SetDefaultContexts(ctxs []string) {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	t.Feature.SetDefaultContexts(ctxs)
+}
+
+// SetDefaultContext sets the default context to the given context string. More
+// than one context can be specified by separating them with semicolon.
+func (t *gofeature) SetDefaultContext(ctx string) {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	t.Feature.SetDefaultContext(ctx)
+}
+
+// SetGlobalContext sets the default global context to the given context.
+func (t *gofeature) SetGlobalContext(ctx string) {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	t.Feature.SetGlobalContext(ctx)
+}
+
+func (t *gofeature) DefaultContext() string {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	return t.Feature.DefaultContext()
+}
+
+func (t *gofeature) States(featureTags, contexts []string) (map[string]*wf.FeatureApplication, error) {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	return t.Feature.States(featureTags, contexts)
+}
+
+func (t *gofeature) State(tag string) (*wf.FeatureApplication, error) {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	return t.Feature.State(tag)
+}
+
+// Bool returns the boolean value of the feature application state for the
+// given tag.
+func (t *gofeature) Bool(tag string) bool {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	return t.Feature.Bool(tag)
+}
+
+// Int returns the integer value of the feature application state for the given
+// tag.
+func (t *gofeature) Int(tag string) int {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	return t.Feature.Int(tag)
+}
+
+// Str returns the string value of the feature application state for the given
+// tag.
+func (t *gofeature) Str(tag string) string {
+	t.ch <- struct{}{}
+	defer func() { <-t.ch }()
+	return t.Feature.Str(tag)
+}
