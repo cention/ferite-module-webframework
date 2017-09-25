c22b08ae323bf15ed1cd18e84e8308c112a7568a
Nazri Ramliy <nazri_ramliy@cention.asia>
2017-09-25 14:56:23 +0800

    Revert "CCC-1571 auth: Accept (and trims!) anything that after (and including) @ in username"
    This reverts commit 30f2a22373cfae29f98186d48989189b19470fc8.
    
    This commit will break the login of existing agents that uses their
    email as their loginname.

--- go/src/c3/auth/auth.go
+++ go/src/c3/auth/auth.go
@@ -197,9 +197,6 @@ func CheckOrCreateAuthCookie(ctx *gin.Context) error {
 	} else {
 		user = strings.TrimSpace(ctx.Request.FormValue("username"))
 		pass = ctx.Request.FormValue("password")
-		if strings.Contains(user, "@") {
-			user = removeSpace(user)
-		}
 		ssid, err = decodeCookie(ctx)
 	}
 
@@ -267,17 +264,6 @@ func CheckOrCreateAuthCookie(ctx *gin.Context) error {
 		}
 	}
 	return ERROR_WF_USER_NULL
-}
-
-// removeSpace removes anything that comes after and including the "@"
-// character in username. Note: 'Space' here does not mean the space character
-// (' ', 0x20).
-func removeSpace(username string) string {
-	i := strings.LastIndex(username, "@")
-	if i < 0 {
-		return username
-	}
-	return username[0:i]
 }
 
 func GetCloudCacheKey(ssid string) string {
